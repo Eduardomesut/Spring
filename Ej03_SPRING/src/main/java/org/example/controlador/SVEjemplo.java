3a4adb3679b4f361c13b080cf02609ebcac8ed62
Eduardo Merino Fernandez <emerino@hiberus.com>
2025-07-17 08:45:36 +0200

    Servlet

--- Ej03_SPRING/src/main/java/org/example/controlador/SVEjemplo.java
+++ Ej03_SPRING/src/main/java/org/example/controlador/SVEjemplo.java
@@ -1,10 +1,13 @@
 package org.example.controlador;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/SVEjemplo")
 public class SVEjemplo implements Servlet {
@@ -27,8 +30,24 @@ public class SVEjemplo implements Servlet {
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        System.out.println("Petición recibida en SVEjemplo");
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SVEjemplo</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>¡Hola desde SVEjemplo!</h1>");
+            out.println("<p>La hora del servidor es: " + new java.util.Date() + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
+
+
+
 
     @Override
     public String getServletInfo() {

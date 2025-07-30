5464914d45b7eca7a7437641f4c011127a98e1c0
Eduardomesut <eduardomesut@gmail.com>
2025-07-30 14:09:49 +0200

    Mejoras y proyecto SpringBoot

---
+++ demo-SpringBoot/src/main/java/org/example/demospringboot/DemoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.example.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+    public static void main(String[] args) {
+        //Aqui hacer proyecto mejorado de películas o sino meter el spring web en otro nuevo
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+    }
+
+}

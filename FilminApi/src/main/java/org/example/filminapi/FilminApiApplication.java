1cf3ef779965a90b081a1ff63272a12ec33c2e98
Eduardomesut <eduardomesut@gmail.com>
2025-07-31 11:08:21 +0200

    Proyecto SpringBoot peliculas

---
+++ FilminApi/src/main/java/org/example/filminapi/FilminApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.filminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilminApiApplication {
+    //Hacerla
+    public static void main(String[] args) {
+        SpringApplication.run(FilminApiApplication.class, args);
+    }
+
+}
---
+++ FilminApi/src/test/java/org/example/filminapi/FilminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.filminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

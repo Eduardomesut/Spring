58c80ca5ef446e14056b59a51cfb6945558f432a
emerinof <emerino@hiberus.com>
2025-08-22 15:52:22 +0200

    Controller locales

--- emerino-microservices/locales/src/main/java/com/hiberus/controladores/LocalController.java
+++ emerino-microservices/locales/src/main/java/com/hiberus/controladores/LocalController.java
@@ -1,9 +1,29 @@
 package com.hiberus.controladores;
 
+import com.hiberus.modelos.Local;
+import com.hiberus.servicios.LocalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/locales")
 public class LocalController {
+
+    @Autowired
+    LocalService localService;
+
+    //Ejemplo en blanco que hay que cambiar
+    @GetMapping("/all")
+    public ResponseEntity<List<Local>> listaLocales(){
+
+        return ResponseEntity.ok(null);
+    }
+
 
 
 }
